c74e4b85eb024d1377e470bd38c7ab3b679c9485
Saketh Sangaraju <technonaut35@gmail.com>
2024-07-09 10:55:21 -0400

    authtoken

---
+++ authtoken.py
@@ -0,0 +1,3 @@
+auth_token = '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'
+groq_token = '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'
+folder_path = r'C:\SAKETH\School\Programming\Envirome\NLP - Image Generation AI\Abstracts_txt'
